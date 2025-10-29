6027a75fffddf5cbec519b4d1b35125989957170
RemiPERRINELLE <remi.perrinelle@gmail.com>
2025-10-29 19:07:07 +0100

    add prisma accelerate to fix prepared statements error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-const db =
+const db: PrismaClient = 
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query", "info", "warn", "error"],
-  });
+  }).$extends(withAccelerate()) as unknown as PrismaClient; // <--- cast
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
 
-export default db
+export default db;
